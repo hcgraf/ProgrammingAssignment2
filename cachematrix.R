67025d42740272bc477ccaff46acf3843b0f58bd
Hagen Graf <hagen@familygraf.de>
2016-01-20 15:53:31 +0100

    completed the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+#  vim: set ts=4 sw=4 sts=4 tw=80 et cc=80 :
 
-## Write a short comment describing this function
-
+## Returns a special "vector" representing a
+## matrix with cacheble inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(i) inv <<- i
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Returns the inverse of a matrix, either the
+## cached version (if available), or the newly computed one
+cacheSolve <- function(x) {
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    mat <- x$get()
+    i <- solve(mat)
+    x$setinverse(i)
+    i
 }
